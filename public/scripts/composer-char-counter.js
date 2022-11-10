140dbf11d6ce23f4f0951c6e5ec455294c41416c
Sara <parakeet_22@yahoo.com>
2022-11-10 17:21:07 -0500

    character counter script added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  console.log('the script is working!');
+  $(".tweet-text").on('input', function(event) {
+    //targeting the text area and getting it's length
+    const charMax = 140;
+    let charCount = $(this).val().length;
+    let remainingChars = charMax - charCount;
+    //finding the counter element and passing it remainingChars as it's text
+    let counter = $(this).parent().find('.counter');
+    counter.text(remainingChars);
+
+    console.log('remaining chars:', remainingChars);
+  
+    //change characters over 140 to red:
+    if (remainingChars < 0) {
+      counter.addClass('turnRed');
+    } else {
+      counter.removeClass('turnRed');
+    }
+  });
+});
