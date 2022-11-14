54a45be2eea7a9d925255de82eea4704c8c5bf15
Sara <parakeet_22@yahoo.com>
2022-11-13 20:52:49 -0500

    Changed post request to an ajax request, and made an ajax get request for the data, downloaded timeago library and replaced function with it, deleted hardcoded data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,76 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
   console.log("script 2 is working!");
-  //Hardcoded temp data
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  };
 
-  const dataBig = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-  //Function to Interpret created_at timestamp
-  const xTimeAgo = function(date) {
-    let seconds = Math.floor((new Date() - date) / 1000);
-
-    let interval = seconds / 31536000;
-
-    if (interval > 1) {
-      return Math.floor(interval) + " years ago";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + " months ago";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + " days ago";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + " hours ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-  };
-
+  //loop through the data to create many tweets
   const renderTweets = function (tweets) {
     let data = tweets[0];
     console.log(`tweetsnot0`, tweets);
@@ -83,6 +15,8 @@ $(document).ready(function() {
     return $("#tweets-container");
   };
 
+
+  //Create one tweet element
   const createTweetElement = function (tweet) {
     let $tweet = `<article class="tweet">
 <header>
@@ -106,7 +40,7 @@ $(document).ready(function() {
 
 <footer>
   <div class="time-stamp">
-    <p>${xTimeAgo(tweet.created_at)}</p>
+    <p>${timeago.format(tweet.created_at)}</p>
   </div>
   <div class="icons">
     <i class="fa-solid fa-flag"></i>
@@ -121,5 +55,28 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  return renderTweets(dataBig);
+  //Stop normal form submission
+  $('form').on('submit', event => {
+    event.preventDefault();
+    let data = $('form').serialize();
+    console.log('DATA', data);
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data
+    });
+  });
+
+  //Load tweets from Database
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      action: "GET"
+    })
+    .then((data) => {
+      renderTweets(data);
+    });
+  }
+
+  return loadTweets();
 });
